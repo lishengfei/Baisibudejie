73f5023e532e09774820cc1491c2d31aa515f855
hrscy <15399070815@163.com>
2016-03-25 08:12:07 +0800

    提交所有

---
+++ Baisibudejie/Classes/Others/YMConst.m
@@ -0,0 +1,22 @@
+
+#import <UIKit/UIKit.h>
+
+CGFloat const YMTitlesViewH = 35;
+CGFloat const YMTitlesViewY = 64;
+
+CGFloat const YMTopicCellMargin = 10;
+CGFloat const YMTopicCellTextY = 70;
+CGFloat const YMTopicCellBottomBarH = 44;
+
+/** 精华-cell-图片帖子的最大高度*/
+CGFloat const YMTopicCellPictureMaxH = 1000;
+/** 精华-cell-图片帖子的一旦超过最大高度，就用Break*/
+CGFloat const YMTopicCellPictureBreakH = 250;
+
+/** 用户模型性别属性值-男*/
+NSString *const YMUserSexMale = @"m";
+/** 用户模型性别属性值-女*/
+NSString *const YMUserSexFamale = @"f";
+/** 最热评论标题的高度*/
+CGFloat const YMTopicCellCmtTitleH = 20;
+
